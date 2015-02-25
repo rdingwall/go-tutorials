150b12b0145312e496fb18cffb76f9aab46770b7
Richard Dingwall <rdingwall@gmail.com>
2015-02-25 14:47:09 +0000

    ch08

---
+++ ch08/q1_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func fibRecursive(n uint) uint {
+	switch n {
+	case 0:
+		return 1
+	case 1:
+		return 2
+	default:
+		return fibRecursive(n-1) + fibRecursive(n-2)
+	}
+}
+
+func TestFibRecursive(t *testing.T) {
+	assert.Equal(t, 1, fibRecursive(0))
+	assert.Equal(t, 2, fibRecursive(1))
+	assert.Equal(t, 3, fibRecursive(2))
+	assert.Equal(t, 5, fibRecursive(3))
+	assert.Equal(t, 8, fibRecursive(4))
+	assert.Equal(t, 13, fibRecursive(5))
+}
+
+func fibIterative(n uint) uint {
+	var sum, prev uint = 1, 1
+	for i := uint(1); i < n+1; i++ {
+		tmp := sum
+		sum += prev
+		prev = tmp
+	}
+	return sum
+}
+
+func TestFibIterative(t *testing.T) {
+	assert.Equal(t, 1, fibIterative(0))
+	assert.Equal(t, 2, fibIterative(1))
+	assert.Equal(t, 3, fibIterative(2))
+	assert.Equal(t, 5, fibIterative(3))
+	assert.Equal(t, 8, fibIterative(4))
+	assert.Equal(t, 13, fibIterative(5))
+}
