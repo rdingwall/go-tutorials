d5fbfdce71e310abd012fcfa8f0bb09d99e9a108
Richard Dingwall <rdingwall@gmail.com>
2015-02-25 14:24:28 +0000

    Pascals triangle in place

--- pascals_triangle_test.go
+++ pascals_triangle_test.go
@@ -4,36 +4,27 @@ import (
 	"testing"
 )
 
-func pascalRow(n int) []int {
-	var row, prevRow []int
-
-	for i := 0; i < n+1; i++ {
-		row = make([]int, i+1)
-
-		for j := 0; j < i+1; j++ {
-			switch j {
-			case 0, i:
-				row[j] = 1
-
-			default:
-				row[j] = prevRow[j-1] + prevRow[j]
-			}
+// Calculate row N of pascal's triangle in place
+func pascalRow(n uint) []uint {
+	row := make([]uint, n+1)
+	for i := 0; i < int(n)+1; i++ {
+		row[i] = 1
+		for j := i - 1; j > 0; j-- {
+			row[j] = row[j] + row[j-1]
 		}
-		
-		prevRow = row
 	}
-
 	return row
 }
 
-func pascalRowR(n int) []int {
+// Calculate row N of Pascal's triangle recursively
+func pascalRowR(n uint) []uint {
 	if n == 0 {
-		return []int{1}
+		return []uint{1}
 	}
 
-	row := make([]int, n+1)
+	row := make([]uint, n+1)
 
-	for i := 0; i < n+1; i++ {
+	for i := uint(0); i < n+1; i++ {
 		switch i {
 		case 0, n:
 			row[i] = 1
@@ -47,7 +38,7 @@ func pascalRowR(n int) []int {
 	return row
 }
 
-func assertEquals(t *testing.T, expected, actual []int) {
+func assertEquals(t *testing.T, expected, actual []uint) {
 	for i := 0; i < len(expected); i++ {
 		if actual[i] != expected[i] {
 			t.Errorf("Expected %v, but got %v", expected, actual)
@@ -57,19 +48,19 @@ func assertEquals(t *testing.T, expected, actual []int) {
 }
 
 func TestPascalRow(t *testing.T) {
-	assertEquals(t, []int{1}, pascalRow(0))
-	assertEquals(t, []int{1, 1}, pascalRow(1))
-	assertEquals(t, []int{1, 2, 1}, pascalRow(2))
-	assertEquals(t, []int{1, 3, 3, 1}, pascalRow(3))
-	assertEquals(t, []int{1, 4, 6, 4, 1}, pascalRow(4))
-	assertEquals(t, []int{1, 5, 10, 10, 5, 1}, pascalRow(5))
+	assertEquals(t, []uint{1}, pascalRow(0))
+	assertEquals(t, []uint{1, 1}, pascalRow(1))
+	assertEquals(t, []uint{1, 2, 1}, pascalRow(2))
+	assertEquals(t, []uint{1, 3, 3, 1}, pascalRow(3))
+	assertEquals(t, []uint{1, 4, 6, 4, 1}, pascalRow(4))
+	assertEquals(t, []uint{1, 5, 10, 10, 5, 1}, pascalRow(5))
 }
 
 func TestPascalRowRecursive(t *testing.T) {
-	assertEquals(t, []int{1}, pascalRowR(0))
-	assertEquals(t, []int{1, 1}, pascalRowR(1))
-	assertEquals(t, []int{1, 2, 1}, pascalRowR(2))
-	assertEquals(t, []int{1, 3, 3, 1}, pascalRowR(3))
-	assertEquals(t, []int{1, 4, 6, 4, 1}, pascalRowR(4))
-	assertEquals(t, []int{1, 5, 10, 10, 5, 1}, pascalRowR(5))
+	assertEquals(t, []uint{1}, pascalRowR(0))
+	assertEquals(t, []uint{1, 1}, pascalRowR(1))
+	assertEquals(t, []uint{1, 2, 1}, pascalRowR(2))
+	assertEquals(t, []uint{1, 3, 3, 1}, pascalRowR(3))
+	assertEquals(t, []uint{1, 4, 6, 4, 1}, pascalRowR(4))
+	assertEquals(t, []uint{1, 5, 10, 10, 5, 1}, pascalRowR(5))
 }
