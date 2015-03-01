0fefc2b31702d089f324e75f778a72a586106c16
Richard Dingwall <rdingwall@gmail.com>
2015-03-01 18:57:20 +0000

    Exercises

--- pascals_triangle_test.go
+++ pascals_triangle_test.go
@@ -4,25 +4,14 @@ import (
 	"testing"
 )
 
-// Calculate row N of pascal's triangle in place
-func pascalRow(n uint) []uint {
-	row := make([]uint, n+1)
-	for i := 0; i < int(n)+1; i++ {
-		row[i] = 1
-		for j := i - 1; j > 0; j-- {
-			row[j] = row[j] + row[j-1]
-		}
-	}
-	return row
-}
+// Calculate row N of Pascal's triangle.
 
-// Calculate row N of Pascal's triangle recursively
-func pascalRowR(n uint) []uint {
+func PascalTriangleRowRecursive(n uint) (row []uint) {
 	if n == 0 {
 		return []uint{1}
 	}
 
-	row := make([]uint, n+1)
+	row = make([]uint, n+1)
 
 	for i := uint(0); i < n+1; i++ {
 		switch i {
@@ -30,37 +19,48 @@ func pascalRowR(n uint) []uint {
 			row[i] = 1
 
 		default:
-			prevRow := pascalRowR(n - 1)
+			prevRow := PascalTriangleRowRecursive(n - 1)
 			row[i] = prevRow[i-1] + prevRow[i]
 		}
 	}
 
-	return row
+	return
 }
 
-func assertEquals(t *testing.T, expected, actual []uint) {
+func TestPascalTriangleRowRecursive(t *testing.T) {
+	assertEqualContents(t, []uint{1}, PascalTriangleRowRecursive(0))
+	assertEqualContents(t, []uint{1, 1}, PascalTriangleRowRecursive(1))
+	assertEqualContents(t, []uint{1, 2, 1}, PascalTriangleRowRecursive(2))
+	assertEqualContents(t, []uint{1, 3, 3, 1}, PascalTriangleRowRecursive(3))
+	assertEqualContents(t, []uint{1, 4, 6, 4, 1}, PascalTriangleRowRecursive(4))
+	assertEqualContents(t, []uint{1, 5, 10, 10, 5, 1}, PascalTriangleRowRecursive(5))
+}
+
+func PascalTriangleRowIterative(n uint) (row []uint) {
+	row = make([]uint, n+1)
+	for i := 0; i < int(n)+1; i++ {
+		row[i] = 1
+		for j := i - 1; j > 0; j-- {
+			row[j] = row[j] + row[j-1]
+		}
+	}
+	return
+}
+
+func TestPascalTriangleRowIterative(t *testing.T) {
+	assertEqualContents(t, []uint{1}, PascalTriangleRowIterative(0))
+	assertEqualContents(t, []uint{1, 1}, PascalTriangleRowIterative(1))
+	assertEqualContents(t, []uint{1, 2, 1}, PascalTriangleRowIterative(2))
+	assertEqualContents(t, []uint{1, 3, 3, 1}, PascalTriangleRowIterative(3))
+	assertEqualContents(t, []uint{1, 4, 6, 4, 1}, PascalTriangleRowIterative(4))
+	assertEqualContents(t, []uint{1, 5, 10, 10, 5, 1}, PascalTriangleRowIterative(5))
+}
+
+func assertEqualContents(t *testing.T, expected, actual []uint) {
 	for i := 0; i < len(expected); i++ {
 		if actual[i] != expected[i] {
 			t.Errorf("Expected %v, but got %v", expected, actual)
 			return
 		}
 	}
-}
-
-func TestPascalRow(t *testing.T) {
-	assertEquals(t, []uint{1}, pascalRow(0))
-	assertEquals(t, []uint{1, 1}, pascalRow(1))
-	assertEquals(t, []uint{1, 2, 1}, pascalRow(2))
-	assertEquals(t, []uint{1, 3, 3, 1}, pascalRow(3))
-	assertEquals(t, []uint{1, 4, 6, 4, 1}, pascalRow(4))
-	assertEquals(t, []uint{1, 5, 10, 10, 5, 1}, pascalRow(5))
-}
-
-func TestPascalRowRecursive(t *testing.T) {
-	assertEquals(t, []uint{1}, pascalRowR(0))
-	assertEquals(t, []uint{1, 1}, pascalRowR(1))
-	assertEquals(t, []uint{1, 2, 1}, pascalRowR(2))
-	assertEquals(t, []uint{1, 3, 3, 1}, pascalRowR(3))
-	assertEquals(t, []uint{1, 4, 6, 4, 1}, pascalRowR(4))
-	assertEquals(t, []uint{1, 5, 10, 10, 5, 1}, pascalRowR(5))
 }
